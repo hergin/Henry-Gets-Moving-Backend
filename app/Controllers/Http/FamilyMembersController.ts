04e62b1f40b8b8090c1177709ca8be125a75a6e6
Mars <ngwilliams@bsu.edu>
2022-10-23 01:00:35 -0400

    Create store function in FamilyMembersController.ts

---
+++ app/Controllers/Http/FamilyMembersController.ts
@@ -0,0 +1,25 @@
+// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+import {HttpContextContract} from "@ioc:Adonis/Core/HttpContext";
+import {schema} from "@ioc:Adonis/Core/Validator";
+import User from "App/Models/User";
+import FamilyMember from "App/Models/FamilyMember";
+
+export default class FamilyMembersController {
+    public async store({ request }: HttpContextContract) {
+        const familyMemberSchema = schema.create({
+            name: schema.string(),
+            userID: schema.number(),
+        })
+        const familyMemberPayload = await request.validate({schema: familyMemberSchema});
+        const user = await User.findOrFail(familyMemberPayload.userID);
+        const familyMember = new FamilyMember();
+        familyMember.name = familyMemberPayload.name;
+        familyMember.userID = user.id;
+
+        await familyMember.save();
+
+        return familyMember;
+
+    }
+}
