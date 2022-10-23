2974b95d66aee2124abef30141f4ff3564260ca4
Mars <ngwilliams@bsu.edu>
2022-10-22 22:22:18 -0400

    Add Users Table and Authentication Middleware

---
+++ app/Models/User.ts
@@ -0,0 +1,19 @@
+import { DateTime } from 'luxon'
+import { column, BaseModel } from '@ioc:Adonis/Lucid/Orm'
+
+export default class User extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public email: string
+
+  @column()
+  public rememberMeToken: string | null
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
