2167b97266852ff538176d8a7d2cc4f499de7666
melody <melodylynnnh@gmail.com>
2022-10-23 14:19:08 -0400

    add beginnings of User tests

--- tests/functional/user.spec.ts
+++ tests/functional/user.spec.ts
@@ -1,5 +1,20 @@
 import { test } from '@japa/runner'
+import UserFactory from '../../database/factories/UserFactory';
+
 
 test.group('User', () => {
-  // TODO: implement
+  test('stores user info in the database', async ({
+    assert, client
+  }) => {
+    let user = await UserFactory.create();
+    const result = await client.post('/user').send(user).end();
+    result.assertStatus(201);
+
+    assert.isDefined(result.body.id);
+    assert.isDefined(result.body.email);
+    assert.isDefined(result.body.rememberMeToken);
+    assert.isDefined(result.body.createdAt);
+    assert.isDefined(result.body.updatedAt);
+    assert.isDefined(result.body.familyMembers);
+  }).timeout(0)
 })
