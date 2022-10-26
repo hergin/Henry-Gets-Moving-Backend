3d98131bb91b630706511fba5ddc31ce044d27ee
Mars <ngwilliams@bsu.edu>
2022-10-26 19:58:17 -0400

    Create Exercise Log migration, model, and controller

---
+++ app/Models/ExerciseLog.ts
@@ -0,0 +1,24 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class ExerciseLog extends BaseModel {
+    @column({ isPrimary: true })
+    public id: number
+
+    @column()
+    public type: string
+
+    @column()
+    public intensity: string
+
+    @column()
+    public duration: number
+
+    @column()
+
+    @column.dateTime({ autoCreate: true })
+    public createdAt: DateTime
+
+    @column.dateTime({ autoCreate: true, autoUpdate: true })
+    public updatedAt: DateTime
+}
---
+++ database/migrations/1666621155052_exercise_logs.ts
@@ -0,0 +1,25 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+    protected tableName = 'exercise_logs'
+
+    public async up() {
+        this.schema.createTable(this.tableName, (table) => {
+            table.increments('id')
+            table.string("type").notNullable()
+            table.enum("intensity", ["Light", "Moderate", "Vigorous"]).notNullable()
+            table.integer("duration").notNullable()
+            table.integer("familyMemberID").notNullable().unsigned().references("family_members.id")
+
+            /**
+             * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+             */
+            table.timestamp('created_at', { useTz: true }).notNullable().defaultTo(this.now())
+            table.timestamp('updated_at', { useTz: true }).notNullable().defaultTo(this.now())
+        })
+    }
+
+    public async down() {
+        this.schema.dropTable(this.tableName)
+    }
+}
