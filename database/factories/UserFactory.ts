819f52a876171ef541a1ffca2cb80d1abf52121d
melody <melodylynnnh@gmail.com>
2022-10-27 15:34:16 -0400

    update user factory

--- database/factories/UserFactory.ts
+++ database/factories/UserFactory.ts
@@ -1,13 +1,11 @@
 import User from 'App/Models/User'
 import Factory from '@ioc:Adonis/Lucid/Factory'
+import FamilyMemberFactory from './FamilyMemberFactory';
 
 export default Factory.define(User, ({faker})=>{
   return {
     id: faker.datatype.number(),
     email: faker.internet.email(),
     rememberMeToken: faker.datatype.string(),
-    createdAt: faker.date.recent(),
-    updatedAt: faker.date.recent(),
-    familyMembers: faker.datatype.array(),
   };
-}).build()
+}).relation('familyMembers',()=>FamilyMemberFactory).build();
