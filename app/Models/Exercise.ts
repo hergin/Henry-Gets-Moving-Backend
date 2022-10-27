5a76e12939d7c7b3dff1a3affe11b3714905c1c5
melody <melodylynnnh@gmail.com>
2022-10-27 16:40:33 -0400

    add columns to exercise model

--- app/Models/Exercise.ts
+++ app/Models/Exercise.ts
@@ -1,13 +1,30 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, HasMany } from '@ioc:Adonis/Lucid/Orm'
+import {ExerciseCategory} from 'App/Models/ExerciseCategory'
 
 export default class Exercise extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public name: string
+
+  @column()
+  public videoLink: string | null
+
+  @column()
+  public isFeatured: boolean
+
+  @column()
+  public categoryID: number
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  @hasMany(() => ExerciseCategory)
+  public categories: HasMany<typeof ExerciseCategory>
+
 }
