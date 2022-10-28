a2bda3e817e0e7af3fa936dff03424c6d0826792
melody <melodylynnnh@gmail.com>
2022-10-28 13:09:14 -0400

    add columns

--- app/Models/RecipeCategory.ts
+++ app/Models/RecipeCategory.ts
@@ -1,13 +1,20 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, HasMany, hasMany } from '@ioc:Adonis/Lucid/Orm'
+import Recipe from 'App/Models/Recipe'
 
 export default class RecipeCategory extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public name: string
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  @hasMany(() => Recipe)
+  public recipe: HasMany<typeof Recipe>
 }
