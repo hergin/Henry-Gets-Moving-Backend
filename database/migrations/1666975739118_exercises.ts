e2bfa5582f3fdf7827abde34e1df5efa309b7155
Mars <ngwilliams@bsu.edu>
2022-10-28 12:51:55 -0400

    Recreate exercise migration

---
+++ database/migrations/1666975739118_exercises.ts
@@ -0,0 +1,29 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+    protected tableName = 'exercises'
+
+    public async up() {
+        this.schema.createTable(this.tableName, (table) => {
+            table.increments('id')
+            table.string('name').notNullable()
+            table.string('videoLink')
+            table.boolean('isFeatured').notNullable().defaultTo(false)
+            table
+                .integer('categoryID')
+                .notNullable()
+                .unsigned()
+                .references('exercise_categories.id')
+
+            /**
+             * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+             */
+            table.timestamp('created_at', { useTz: true }).notNullable().defaultTo(this.now())
+            table.timestamp('updated_at', { useTz: true }).notNullable().defaultTo(this.now())
+        })
+    }
+
+    public async down() {
+        this.schema.dropTable(this.tableName)
+    }
+}
