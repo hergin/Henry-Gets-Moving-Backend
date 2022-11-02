e632124f7e3d5b004e86e980e690bee5cbe59857
Mars <ngwilliams@bsu.edu>
2022-11-02 01:16:23 -0400

    Add store function to RecipeCategoriesController.ts

--- app/Controllers/Http/RecipeCategoriesController.ts
+++ app/Controllers/Http/RecipeCategoriesController.ts
@@ -1,3 +1,24 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { schema } from '@ioc:Adonis/Core/Validator'
+import RecipeCategory from 'App/Models/RecipeCategory'
 
-export default class RecipeCategoriesController {}
+export default class RecipeCategoriesController {
+    public async store({ request, response }: HttpContextContract) {
+        const recipeCategorySchema = schema.create({
+            name: schema.string(),
+        })
+
+        const requestBody = await request.validate({ schema: recipeCategorySchema })
+
+        if ((await RecipeCategory.findBy('name', requestBody.name)) !== null) {
+            response.badRequest('Recipe Category already exists')
+            return
+        }
+
+        const recipeCategory = new RecipeCategory()
+        recipeCategory.name = requestBody.name
+
+        await recipeCategory.save()
+        return recipeCategory
+    }
+}
