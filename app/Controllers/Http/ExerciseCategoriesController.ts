d93eeb27a19b0263db5e94b3eb9172f97352ab5c
Mars <ngwilliams@bsu.edu>
2022-11-02 01:53:00 -0400

    Add store function to ExerciseCategoriesController.ts

--- app/Controllers/Http/ExerciseCategoriesController.ts
+++ app/Controllers/Http/ExerciseCategoriesController.ts
@@ -1,3 +1,19 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { schema } from '@ioc:Adonis/Core/Validator'
+import ExerciseCategory from 'App/Models/ExerciseCategory'
 
-export default class ExerciseCategoriesController {}
+export default class ExerciseCategoriesController {
+    public async store({ request }: HttpContextContract) {
+        const exerciseCategorySchema = schema.create({
+            name: schema.string(),
+        })
+
+        const exerciseCategoryPayload = await request.validate({ schema: exerciseCategorySchema })
+
+        const exerciseCategory = new ExerciseCategory()
+        exerciseCategory.name = exerciseCategoryPayload.name
+
+        await exerciseCategory.save()
+        return exerciseCategory
+    }
+}
