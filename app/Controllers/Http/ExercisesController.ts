f4366efcb01972204802eafb8a8e55aff81c8d10
Mars <ngwilliams@bsu.edu>
2022-11-03 13:20:44 -0400

    Add update function to ExercisesController.ts

--- app/Controllers/Http/ExercisesController.ts
+++ app/Controllers/Http/ExercisesController.ts
@@ -47,6 +47,28 @@ export default class ExercisesController {
         return Exercise.query().where('id', params.id).preload('exerciseCategory')[0]
     }
 
+    public async update({ params, request }: HttpContextContract) {
+        const exerciseSchema = schema.create({
+            name: schema.string({ trim: true }),
+            videoLink: schema.string({ trim: true }),
+            isFeatured: schema.boolean(),
+            category_id: schema.number(),
+        })
+
+        const requestBody = await request.validate({ schema: exerciseSchema })
+
+        const name = requestBody.name
+
+        const exercise = await Exercise.findOrFail(params.id)
+        exercise.name = name
+        exercise.videoLink = requestBody.videoLink
+        exercise.isFeatured = requestBody.isFeatured
+        exercise.category_id = requestBody.category_id
+
+        await exercise.save()
+        return exercise
+    }
+
     public async destroy({ params }: HttpContextContract) {
         let exercise = await Exercise.findOrFail(params.id)
         await exercise.delete()
