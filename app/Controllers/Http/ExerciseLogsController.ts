b04a84665589d09e4dba4e0ee41b3b1f0d9ff1ca
Mars <ngwilliams@bsu.edu>
2022-11-02 22:31:24 -0400

    Add store function to ExerciseLogsController.ts

--- app/Controllers/Http/ExerciseLogsController.ts
+++ app/Controllers/Http/ExerciseLogsController.ts
@@ -1,3 +1,26 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { schema } from '@ioc:Adonis/Core/Validator'
+import ExerciseLog from 'App/Models/ExerciseLog'
 
-export default class ExerciseLogsController {}
+export default class ExerciseLogsController {
+    public async store({ request }: HttpContextContract) {
+        const exerciseLogSchema = schema.create({
+            type: schema.string(),
+            intensity: schema.string(),
+            duration: schema.number(),
+            familyMemberID: schema.number(),
+        })
+
+        const exerciseLogPayload = await request.validate({ schema: exerciseLogSchema })
+
+        const exerciseLog = new ExerciseLog()
+
+        exerciseLog.type = exerciseLogPayload.type
+        exerciseLog.intensity = exerciseLogPayload.intensity
+        exerciseLog.duration = exerciseLogPayload.duration
+        exerciseLog.familyMemberID = exerciseLogPayload.familyMemberID
+
+        await exerciseLog.save()
+        return exerciseLog
+    }
+}
