fd10c7c2ade1c78d241fe440ae8f374852f93b21
melody <melodylynnnh@gmail.com>
2022-11-04 13:03:03 -0400

    add `setup` method and test invalid user creation

--- tests/functional/user.spec.ts
+++ tests/functional/user.spec.ts
@@ -1,14 +1,18 @@
+import Database from '@ioc:Adonis/Lucid/Database'
 import { test } from '@japa/runner'
 
+test.group('User store', (group) => {
+    group.each.setup( async () => {
+        await Database.beginGlobalTransaction()
+
+        return () => Database.rollbackGlobalTransaction()
+    })
+})
+
 test.group('User', () => {
-    test('stores user info in the database', async ({ assert, client }) => {
-        const result = await client.post('/users')
-        assert.isDefined(result)
-        // assert.isDefined(result.body.id)
-        // assert.isDefined(result.body.email)
-        // assert.isDefined(result.body.rememberMeToken)
-        // assert.isDefined(result.body.createdAt)
-        // assert.isDefined(result.body.updatedAt)
-        // assert.isDefined(result.body.familyMembers)
-    }).timeout(0)
+    test('can\'t create user without email', async ({ assert, client, route }) => {
+        const result = await client.post(route('UsersController.store'))
+        result.assertStatus(422)
+        result.assertBodyContains({errors: [{message:'required validation failed',rule:'required',field:'email'}]})
+    })
 })
