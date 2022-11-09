e3ecf5c6fb66aeef7474da7ced897b70b3d9a8c0
melody <melodylynnnh@gmail.com>
2022-11-09 15:20:36 -0500

    up[date family member tests

--- tests/functional/family_member.spec.ts
+++ tests/functional/family_member.spec.ts
@@ -1,9 +1,9 @@
 import { test } from '@japa/runner'
 
 test.group('Family member', () => {
-  test('can\'t create family member without login', async ({ assert, client, route }) => {
-      const result = await client.post(route('UsersController.store'))
+  test('can\'t create family member without required attributes', async ({ assert, client, route }) => {
+      const result = await client.post(route('FamilyMembersController.store'))
       result.assertStatus(422)
-      result.assertBodyContains({errors: [{message:'required validation failed',rule:'required',field:'email'}]})
+      result.assertBodyContains({errors: [{message:'required validation failed',rule:'required',field:'name',field:'user_id'}]})
   })
 })
