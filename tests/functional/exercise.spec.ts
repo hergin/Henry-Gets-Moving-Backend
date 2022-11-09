4d0d02dd5d85e724455050430b9b4586c75b1114
melody <melodylynnnh@gmail.com>
2022-11-09 15:23:01 -0500

    update exercise test

--- tests/functional/exercise.spec.ts
+++ tests/functional/exercise.spec.ts
@@ -1,9 +1,9 @@
 import { test } from '@japa/runner'
 
 test.group('Exercise', () => {
-  test('exercise view', async ({ assert, client, route }) => {
-      const result = await client.post(route('UsersController.store'))
+  test('can\'t create exercises without required fields', async ({ assert, client, route }) => {
+      const result = await client.post(route('ExercisesController.store'))
       result.assertStatus(422)
-      result.assertBodyContains({errors: [{message:'required validation failed',rule:'required',field:'email'}]})
+      result.assertBodyContains({errors: [{message:'required validation failed',rule:'required',field:'name'}]})
   })
 })
