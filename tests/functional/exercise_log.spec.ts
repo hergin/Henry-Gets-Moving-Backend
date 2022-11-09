6baa15e9d6a25cd0736dd1a634f73d9dd251f581
melody <melodylynnnh@gmail.com>
2022-11-09 15:00:23 -0500

    add exercise log test

--- tests/functional/exercise_log.spec.ts
+++ tests/functional/exercise_log.spec.ts
@@ -1,5 +1,9 @@
 import { test } from '@japa/runner'
 
 test.group('Exercise log', () => {
-    // Write your test here
+    test('can\'t create exercise log without login', async ({ assert, client, route }) => {
+        const result = await client.post(route('UsersController.store'))
+        result.assertStatus(422)
+        result.assertBodyContains({errors: [{message:'required validation failed',rule:'required',field:'email'}]})
+    })
 })
