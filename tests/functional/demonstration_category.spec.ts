6ba4c4518e5b0180770c5b0f914b8af912d08f15
melody <melodylynnnh@gmail.com>
2022-11-29 13:28:04 -0500

    add demonstration category test

--- tests/functional/demonstration_category.spec.ts
+++ tests/functional/demonstration_category.spec.ts
@@ -2,8 +2,8 @@ import { test } from '@japa/runner'
 
 test.group('Demonstration category', () => {
   test('can\'t create category without required fields', async ({ assert, client, route }) => {
-      const result = await client.post(route('UsersController.store'))
+      const result = await client.post(route('DemonstrationCategoriesController.store'))
       result.assertStatus(422)
-      result.assertBodyContains({errors: [{message:'required validation failed',rule:'required',field:'email'}]})
+      result.assertBodyContains({errors: [{message:'required validation failed',rule:'required',field:'name'}]})
   })
 })
