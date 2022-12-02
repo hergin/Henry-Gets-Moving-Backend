ce0ea0c7fae77198988cec18282e3d730cc7d89b
Mars <ngwilliams@bsu.edu>
2022-12-01 22:24:08 -0500

    Add store function to DiagramsController.ts

--- app/Controllers/Http/DiagramsController.ts
+++ app/Controllers/Http/DiagramsController.ts
@@ -1,8 +1,24 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Diagram from 'App/Models/Diagram'
+import { schema } from '@ioc:Adonis/Core/Validator'
 
 export default class DiagramsController {
     public async index({}: HttpContextContract) {
         return Diagram.query().orderBy('updated_at')
+    }
+
+    public async store({ request }: HttpContextContract) {
+        const diagramSchema = schema.create({
+            thumbnail_link: schema.string({ trim: true }),
+        })
+
+        const diagramPayload = await request.validate({ schema: diagramSchema })
+        const diagram = new Diagram()
+
+        diagram.thumbnail_link = diagramPayload.thumbnail_link
+
+        await diagram.save()
+
+        return diagram
     }
 }
