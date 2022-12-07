7f5da79fff3fc058f446b3edf5ae556cce00dca0
Mars <ngwilliams@bsu.edu>
2022-12-07 11:58:34 -0500

    Add prep time to recipes & change steps/ingredients to text

--- database/migrations/1666976975639_recipes.ts
+++ database/migrations/1666976975639_recipes.ts
@@ -9,11 +9,12 @@ export default class extends BaseSchema {
 
             table.string('name').notNullable()
             table.string('thumbnail').notNullable()
-            table.string('cook_time').notNullable()
-            table.string('ingredients').notNullable()
-            table.string('recipe_steps').notNullable()
+            table.string('cook_time')
+            table.text('ingredients', 'text').notNullable()
+            table.text('recipe_steps', 'text').notNullable()
             table.integer('category_id').notNullable().unsigned().references('recipe_categories.id')
             table.boolean('is_featured').notNullable().defaultTo(false)
+            table.string('prep_time')
 
             table.timestamp('created_at', { useTz: true }).notNullable().defaultTo(this.now())
             table.timestamp('updated_at', { useTz: true }).notNullable().defaultTo(this.now())
