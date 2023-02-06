3e9417c591992b30c823e5185bfa3c384dea55ad
melody <melodylynnnh@gmail.com>
2023-02-06 16:29:36 -0500

    add setup method exercise

--- tests/functional/exercise.spec.ts
+++ tests/functional/exercise.spec.ts
@@ -1,6 +1,11 @@
 import { test } from '@japa/runner'
 
-test.group('Exercise', () => {
+test.group('Exercise', (group) => {
+        group.each.setup(async () => {
+            await Database.beginGlobalTransaction()
+    
+            return () => Database.rollbackGlobalTransaction()
+        })
     test("can't create exercises without required fields", async ({ assert, client, route }) => {
         const result = await client.post(route('ExercisesController.store'))
         result.assertStatus(422)
