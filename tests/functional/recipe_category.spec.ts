524061ecc1d09b434647dd3ec25594442760ed7a
melody <melodylynnnh@gmail.com>
2023-02-06 17:41:07 -0500

    add recipecategory create + show test

--- tests/functional/recipe_category.spec.ts
+++ tests/functional/recipe_category.spec.ts
@@ -1,15 +1,27 @@
+import Database from '@ioc:Adonis/Lucid/Database'
 import { test } from '@japa/runner'
 
-test.group('Recipe Category', () => {
-    test("can't create recipe category without required fields", async ({
-        assert,
-        client,
-        route,
-    }) => {
+test.group('Recipe Category', (group) => {
+    group.each.setup(async () => {
+        await Database.beginGlobalTransaction()
+
+        return () => Database.rollbackGlobalTransaction()
+    })
+    test("can't create recipe category without required fields", async ({client,route}) => {
         const result = await client.post(route('RecipeCategoriesController.store'))
         result.assertStatus(422)
         result.assertBodyContains({
             errors: [{ message: 'required validation failed', rule: 'required', field: 'name' }],
         })
+    })
+    test("create and show recipecategory", async ({client, route}) => {
+        const categoryToCreate = {
+            name: "Test Recipe Category",
+        }
+        const postResult = await client.post(route('RecipeCategoriesController.store')).form(categoryToCreate)
+        postResult.assertStatus(200)
+        const result = await client.get(route('RecipeCategoriesController.show', {id: postResult.body().id}))
+        result.dump()
+        result.assertBodyContains(categoryToCreate)
     })
 })
