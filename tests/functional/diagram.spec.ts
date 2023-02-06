0fb15f11b800f0605ad9e5b556f8ccb9c90c488e
melody <melodylynnnh@gmail.com>
2023-02-06 17:46:11 -0500

    fix compiler errors

--- tests/functional/diagram.spec.ts
+++ tests/functional/diagram.spec.ts
@@ -1,6 +1,7 @@
+import Database from '@ioc:Adonis/Lucid/Database'
 import { test } from '@japa/runner'
 
-test.group('Diagrams', () => {
+test.group('Diagrams', (group) => {
     group.each.setup(async () => {
         await Database.beginGlobalTransaction()
 
