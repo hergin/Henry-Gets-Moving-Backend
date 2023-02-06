42d78ec058b4a511fa6d50edb53abae94c61682f
melody <melodylynnnh@gmail.com>
2023-02-06 15:49:06 -0500

    add back beginnings of user tests

--- tests/functional/user.spec.ts
+++ tests/functional/user.spec.ts
@@ -4,10 +4,24 @@ import User from 'App/Models/User'
 
 
 
-test.group('User', () => {
+test.group('User', (group) => {
+
+    group.each.setup(async () => {
+        await Database.beginGlobalTransaction()
+
+        return () => Database.rollbackGlobalTransaction()
+    })
+
     test('can\'t create user without email', async ({ assert, client, route }) => {
         const result = await client.post(route('UsersController.store'))
         result.assertStatus(422)
         result.assertBodyContains({errors: [{message:'required validation failed',rule:'required',field:'email'}]})
+    })
+
+    test('user creation works', async ({ assert, client, route }) => {
+        const result = await client.post(route('UsersController.store')).form({
+            email: 'test@bsu.edu',
+        })
+        result.dumpBody()
     })
 })
