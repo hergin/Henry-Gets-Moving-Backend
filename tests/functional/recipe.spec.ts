00bdcb30444443364faa374bd94ddeb15756a911
melody <melodylynnnh@gmail.com>
2023-02-06 17:29:03 -0500

    add setup method and create + show recipe test

--- tests/functional/recipe.spec.ts
+++ tests/functional/recipe.spec.ts
@@ -1,11 +1,34 @@
+import Database from '@ioc:Adonis/Lucid/Database'
 import { test } from '@japa/runner'
 
-test.group('Recipe', () => {
+test.group('Recipe', (group) => {
+    group.each.setup(async () => {
+        await Database.beginGlobalTransaction()
+
+        return () => Database.rollbackGlobalTransaction()
+    })
+
     test("can't create recipe without required fields", async ({ assert, client, route }) => {
         const result = await client.post(route('RecipesController.store'))
         result.assertStatus(422)
         result.assertBodyContains({
             errors: [{ message: 'required validation failed', rule: 'required', field: 'name' }],
         })
+    })
+    test("create and show recipe", async ({client, route}) => {
+        const recipeToCreate = {
+            name: "Test Recipe",
+            thumbnail: "test.com",
+            cook_time: "8 years",
+            is_featured: 0,
+            category_id: 1,
+            prep_time: "20 minutes",
+            recipe_steps: "step 1. cook",
+            ingredients: "banana"
+        }
+        const postResult = await client.post(route('RecipesController.store')).form(recipeToCreate)
+        postResult.assertStatus(200)
+        const result = await client.get(route('RecipesController.show', {id: postResult.body().id}))
+        result.assertBodyContains(recipeToCreate)
     })
 })
