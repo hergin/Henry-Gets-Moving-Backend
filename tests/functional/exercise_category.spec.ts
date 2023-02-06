8407d34837eb32232997f1171480a875b986eec7
melody <melodylynnnh@gmail.com>
2023-02-06 17:49:05 -0500

    add setup method exercise category

--- tests/functional/exercise_category.spec.ts
+++ tests/functional/exercise_category.spec.ts
@@ -1,11 +1,13 @@
+import Database from '@ioc:Adonis/Lucid/Database'
 import { test } from '@japa/runner'
 
-test.group('Exercise category', () => {
-    test("can't create exercise category without required fields", async ({
-        assert,
-        client,
-        route,
-    }) => {
+test.group('Exercise category', (group) => {
+    group.each.setup(async () => {
+        await Database.beginGlobalTransaction()
+
+        return () => Database.rollbackGlobalTransaction()
+    })
+    test("can't create exercise category without required fields", async ({client,route}) => {
         const result = await client.post(route('ExerciseCategoriesController.store'))
         result.assertStatus(422)
         result.assertBodyContains({
