12b1aabcc1cedb8a20d8427520acb5d596a05cb5
melody <melodylynnnh@gmail.com>
2023-02-07 13:02:21 -0500

    add recipecategory seeder

---
+++ database/seeders/RecipeCategory.ts
@@ -0,0 +1,15 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import RecipeCategory from 'App/Models/RecipeCategory'
+
+export default class extends BaseSeeder {
+  public async run () {
+    await RecipeCategory.createMany([
+      {
+        name: "Test recipecategory"
+      },
+      {
+        name: "Tets recipe Category 2"
+      }
+    ])
+  }
+}
