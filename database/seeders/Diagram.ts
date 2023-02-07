42ed8dd691f25a71f1c5355922b46c2347740bc2
melody <melodylynnnh@gmail.com>
2023-02-07 13:06:34 -0500

    add diagram seeder:

---
+++ database/seeders/Diagram.ts
@@ -0,0 +1,17 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import Diagram from 'App/Models/Diagram'
+
+export default class extends BaseSeeder {
+  public async run () {
+    await Diagram.createMany([
+      {
+        name: "Test diagram 1",
+        thumbnail_link: "image.com"
+      },
+      {
+        name: "Test diagram 2",
+        thumbnail_link: "image_also.com"
+      }
+    ])
+  }
+}
