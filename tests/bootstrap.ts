ea3cff4a6e5f0caefff5b981af83fafc020b767f
melody <melodylynnnh@gmail.com>
2023-02-07 13:11:32 -0500

    add database seed runner hook:

--- tests/bootstrap.ts
+++ tests/bootstrap.ts
@@ -56,7 +56,7 @@ export const reporters: Config['reporters'] = [specReporter()]
 |
 */
 export const runnerHooks: Required<Pick<Config, 'setup' | 'teardown'>> = {
-    setup: [() => TestUtils.ace().loadCommands(), () => TestUtils.db().migrate()],
+    setup: [() => TestUtils.ace().loadCommands(), () => TestUtils.db().migrate(), () => TestUtils.db().seed()],
     teardown: [],
 }
 
