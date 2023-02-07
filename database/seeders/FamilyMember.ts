222650d7882f970a1a6aa4710e5c953db8982682
melody <melodylynnnh@gmail.com>
2023-02-07 13:10:38 -0500

    add family member seeder

---
+++ database/seeders/FamilyMember.ts
@@ -0,0 +1,17 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import FamilyMember from 'App/Models/FamilyMember'
+
+export default class extends BaseSeeder {
+  public async run () {
+    await FamilyMember.createMany([
+      {
+        name: "Test child",
+        user_id: 1
+      },
+      {
+        name: "test child 2",
+        user_id: 2
+      }
+    ])
+  }
+}
