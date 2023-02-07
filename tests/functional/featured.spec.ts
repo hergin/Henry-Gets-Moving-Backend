33eb7980879fe0bc5d601ba4d1fe3bf7e05a2239
melody <melodylynnnh@gmail.com>
2023-02-06 20:37:10 -0500

    add setup method featured

--- tests/functional/featured.spec.ts
+++ tests/functional/featured.spec.ts
@@ -1,5 +1,10 @@
+import Database from '@ioc:Adonis/Lucid/Database'
 import { test } from '@japa/runner'
 
-test.group('Featured', () => {
-    // Write your test here
+test.group('Featured', (group) => {
+    group.each.setup(async () => {
+        await Database.beginGlobalTransaction()
+
+        return () => Database.rollbackGlobalTransaction()
+    })
 })
