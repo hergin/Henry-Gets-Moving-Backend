bfe5d8bc9050e1480a8b5aaa97dfd340463bf778
Mars <ngwilliams@bsu.edu>
2023-02-07 22:51:20 -0500

    Add check family member tests to family_member.spec.ts

--- tests/functional/family_member.spec.ts
+++ tests/functional/family_member.spec.ts
@@ -10,19 +10,35 @@ test.group('Family member', () => {
             errors: [{ message: 'E_UNAUTHORIZED_ACCESS: Unauthorized access' }],
         })
     })
-    test('create family member with authentication', async ({ assert, client, route }) => {
+    test('check existing family member', async ({ assert, client, route }) => {
         assert.plan(2)
         const user = await User.find(1)
         const result = await client
-            .post(route('FamilyMembersController.store'))
+            .post(route('FamilyMembersController.checkFamilyMember'))
             .loginAs(user!)
             .form({
-                name: 'Test',
+                name: 'Test child',
                 user_id: 1,
             })
         result.assertStatus(200)
         result.assertBodyContains({
-            name: 'Test',
+            name: 'Test child',
+            user_id: 1,
+        })
+    })
+    test('check non-existing family member', async ({ assert, client, route }) => {
+        assert.plan(2)
+        const user = await User.find(1)
+        const result = await client
+            .post(route('FamilyMembersController.checkFamilyMember'))
+            .loginAs(user!)
+            .form({
+                name: 'Test child not exist',
+                user_id: 1,
+            })
+        result.assertStatus(200)
+        result.assertBodyContains({
+            name: 'Test child not exist',
             user_id: 1,
         })
     })
