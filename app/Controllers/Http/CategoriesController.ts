5bb1da3abdac217b8cbdcafa21e6a55ebcc00aab
Mars <ngwilliams@bsu.edu>
2023-03-20 05:18:58 -0400

    Create setRecipeCategories method in CategoriesController.ts

--- app/Controllers/Http/CategoriesController.ts
+++ app/Controllers/Http/CategoriesController.ts
@@ -1,5 +1,6 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Exercise from 'App/Models/Exercise'
+import Recipe from 'App/Models/Recipe'
 
 export default class CategoriesController {
     public async setExerciseCategories({ params, request }: HttpContextContract) {
@@ -7,5 +8,12 @@ export default class CategoriesController {
         const categoriesIDS = request.body()
         await exercise.related('exerciseCategories').sync(categoriesIDS)
         return exercise.exerciseCategories
+    }
+
+    public async setRecipeCategories({ params, request }: HttpContextContract) {
+        const recipe = await Recipe.findOrFail(params.id)
+        const categoriesIDS = request.body()
+        await recipe.related('recipeCategories').sync(categoriesIDS)
+        return recipe.recipeCategories
     }
 }
